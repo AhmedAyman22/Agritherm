9afdc307e56858c3172c0408bc04089094c6eb92
AhmAyman <Ahm.Ayman4566@gmail.com>
2023-05-28 13:14:58 +0200

    Adding the Country column in cities dataset

--- Agritherm_data/models.py
+++ Agritherm_data/models.py
@@ -5,7 +5,7 @@ class City(models.Model):
     name = models.CharField(max_length = 25, primary_key = True)
     lat =  models.FloatField()
     lon =  models.FloatField()
+    country = models.CharField(max_length = 25, default='')
     
     def __str__(self):
         return self.name
-
